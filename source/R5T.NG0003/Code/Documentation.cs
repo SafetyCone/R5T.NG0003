40cedae87f8b09b5d40f5630bfaca1133db44cb3
davidcoats <david@getsafetycone.com>
2022-09-26 10:29:34 -0700

    Initial commit.

---
+++ source/R5T.NG0003/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0003
+{
+	/// <summary>
+	/// Microsoft.Extensions.Logging.Console NuGet package selector library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
